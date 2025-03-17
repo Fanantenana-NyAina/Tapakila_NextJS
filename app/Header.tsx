4a628dd7800c291d0ce20a70f6d9ac0d957b6aad
Fanantenana Ny Aina <hei.fanantenana@gmail.com>
2025-03-17 19:19:17 +0300

    feat: made header and hero_section for home page with responsive

---
+++ app/Header.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { IoMenuSharp } from "react-icons/io5";
+import { useState } from "react";
+
+export default function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  return (
+    <header className="w-full bg-[#0a1128] text-white">
+      <nav className="max-w-full flex justify-between items-center py-3 px-6 md:px-16 relative">
+        <div className="flex items-center gap-2 text-white hover:opacity-90 transition-opacity">
+          <Link href="/" className="flex items-center text-white hover:opacity-90 transition-opacity">
+            <Image src="/ETicket_Logo.svg" alt="logo" width={40} height={30} />
+            <span className="text-2xl font-semibold">Tapakila</span>
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-9 ml-auto">
+          <ul className="hidden xl:flex items-center gap-9 font-medium text-base">
+            {["Home", "Events", "About", "Contact"].map((item) => (
+              <li key={item} className="relative cursor-pointer transition-all 
+                before:absolute before:bottom-0 before:left-0 before:w-0 before:h-[2px] 
+                before:bg-blue-400 before:transition-all before:duration-300 before:content-[''] 
+                hover:before:w-full">
+                <Link href={`/${item.toLowerCase().replace(" ", "-").replace("à", "a")}`}>
+                  {item}
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+          <div className="hidden md:flex gap-4">
+            <Link href="/connexion" className="px-5 py-2 border border-[#009de0] text-[#58d0fc] rounded-full font-medium hover:bg-white hover:text-blue-950 transition-all">
+              Connexion
+            </Link>
+            <Link
+              href="/creer-billetterie"
+              className="px-5 py-2 bg-[#009de0] text-white rounded-full font-medium hover:bg-white hover:text-blue-950 transition-all">
+              New coming event ✨
+            </Link>
+          </div>
+
+          <IoMenuSharp className="size-8 xl:hidden block cursor-pointer text-white" onClick={() => setOpenMenu(!openMenu)} />
+        </div>
+      </nav>
+
+      {openMenu && (
+        <nav className="fixed top-0 left-0 right-0 bottom-0 bg-[#0a1128] z-50">
+          <div className="flex justify-between items-center py-3 px-6 md:px-16 border-b border-blue-900">
+            <div className="text-2xl text-white hover:opacity-90 transition-opacity">
+              <Link href="/" className="flex items-center text-white hover:opacity-90 transition-opacity">
+                <Image src="/ETicket_Logo.svg" alt="logo" width={40} height={30} />
+                <span className="font-bold">Tapakila</span>
+              </Link>
+            </div>
+            <IoMenuSharp className="size-8 cursor-pointer text-white" onClick={() => setOpenMenu(false)} />
+          </div>
+
+          <div className="flex flex-col items-center pt-8">
+            {["Nos solutions", "Tarifs", "À propos", "Blog", "Contact"].map((item) => (
+              <Link
+                key={item}
+                href={`/${item.toLowerCase().replace(" ", "-").replace("à", "a")}`}
+                className="w-full text-center py-4 text-xl relative cursor-pointer transition-all 
+                  before:absolute before:bottom-0 before:left-1/4 before:w-0 before:h-[2px] 
+                  before:bg-blue-400 before:transition-all before:duration-300 before:content-[''] 
+                  hover:before:w-1/2"
+                onClick={() => setOpenMenu(false)}
+              >
+                {item}
+              </Link>
+            ))}
+
+            <div className="w-full flex flex-col items-center gap-4 mt-8">
+              <Link
+                href="/connexion"
+                className="px-5 py-2 text-center text-white border border-white rounded-full font-medium hover:bg-white hover:text-blue-950 transition-all"
+                onClick={() => setOpenMenu(false)}
+              >
+                Connexion
+              </Link>
+              <Link
+                href="/creer-billetterie"
+                className="px-5 py-2 bg-[#009de0] text-white rounded-full font-medium hover:bg-white hover:text-blue-950 transition-all">
+                New coming event ✨
+              </Link>
+            </div>
+          </div>
+        </nav>
+      )}
+    </header>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import { FaSearch } from "react-icons/fa";
+
 export default function Home() {
   return (
-    <div className="text-center py-20 z-50">
-      <h1 className="text-3xl font-bold">Bienvenue sur Tapakila</h1>
+    <div className="hero-section flex flex-col justify-center items-center py-16 md:py-44 z-50 h-full pb-48 bg-cover bg-center">
+      <div className="flex flex-col justify-center items-center text-center w-full md:w-3xl px-4 md:px-0">
+        <h1 className="text-4xl sm:text-5xl font-bold font-stretch-expanded mb-3.5 text-white">
+          Welcome to <span className="text-[#009de0]">tapakila</span>
+        </h1>
+        <p className="font-mono mb-4 text-xl sm:text-base md:text-lg text-white">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt
+          deleniti voluptatibus eaque cupiditate maxime tenetur fugiat
+          laboriosam ipsum aut, quisquam dolores? Perferendis delectus expedita
+          excepturi impedit explicabo rem ipsa ipsam!
+        </p>
+      </div>
+      <div className="relative flex items-center justify-center gap-3 w-full max-w-md px-4 mt-4">
+        <FaSearch className="absolute left-7 text-gray-500" />
+        <input
+          type="text"
+          placeholder="Search Event"
+          className="w-full py-2 pl-10 pr-4 rounded-xl border-2 bg-slate-50 border-[#009de0] focus:bg-slate-100 focus:outline-[#0a1128]"
+        />
+      </div>
     </div>
   );
 }
