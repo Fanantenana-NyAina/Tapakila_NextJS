e8c8d4fa6f8a2d7915ec680b6e21aedfa4ac993d
Fanantenana Ny Aina <hei.fanantenana@gmail.com>
2025-03-18 23:54:23 +0300

    fix: separate the result in SearchResult component for better maintainability

--- app/components/SearchResult.tsx
+++ app/components/SearchResult.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function SearchResult({ result }) {
     return (
-        <div>{result.name}</div>
+        <div className="px-4 py-2.5 hover:bg-slate-50 transition-colors duration-200 
+            cursor-pointer border-b border-gray-100 last:border-b-0">
+            {result.name}
+        </div>
     )
 }
--- app/components/SearchResultList.tsx
+++ app/components/SearchResultList.tsx
@@ -1,3 +1,5 @@
+import SearchResult from "./SearchResult";
+
 export default function SearchResultList({ results }: { results: any[] }) {
     if (results.length === 0) return null;
 
@@ -5,10 +7,7 @@ export default function SearchResultList({ results }: { results: any[] }) {
         <div className="absolute top-full left-0 right-0 bg-white max-h-44 overflow-y-auto 
                        border border-[#009de0]/30 shadow-lg rounded-lg mt-2 z-10">
             {results.map((result, id) => (
-                <div key={id} className="px-4 py-2.5 hover:bg-slate-50 transition-colors duration-200 
-                                      cursor-pointer border-b border-gray-100 last:border-b-0">
-                    {result.name}
-                </div>
+                <SearchResult key={id} result={result} />
             ))}
         </div>
     );
