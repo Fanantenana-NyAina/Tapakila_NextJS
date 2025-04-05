da689735fef350a7d49de50053206fcb2ff65f1d
Fanantenana Ny Aina <hei.fanantenana@gmail.com>
2025-04-05 08:12:09 +0300

    fix: lorem ipsum text and rename 'userprofile' to 'userProfile'

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
         <div className='flex flex-col lg:flex-row font-mono text-white about-section'>
             <div className='w-full flex justify-center items-center flex-col-reverse gap-10'>
                 <div className='text-xl md:text-2xl font-bold mb-3 bg-[#125876cd] h-18 md:w-fit w-80 md:px-3 py-5 rounded-full'>
-                    <h1 className='text-center'>What we really <span className='font-bold text-[#009de0]'>Do </span> ?</h1>
+                    <h1 className='text-center'>Who we really <span className='font-bold text-[#009de0]'>Are </span> ?</h1>
                 </div>
 
                 <div className='border-[#009de0] border-3 rounded-full w-fit mt-18'>
@@ -25,8 +25,8 @@ export default function About() {
                         <span className="underline">Know us more!</span>
                     </h1>
                     <p className='text-[0.88rem] lg:text-xl text-center lg:w-2xl'>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit voluptatem vel ipsam ipsum quam dolore sapiente in sunt ut! Nisi inventore molestiae laborum soluta, aliquam voluptates consequatur quae dicta quos.
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod expedita neque odit aliquam maxime nobis hic, nesciunt excepturi earum ea modi reiciendis natus in vitae, est vero? Quisquam, quasi perspiciatis!
+                        Tapakila is more than just a ticketing platform—we’re your gateway to unforgettable experiences. We connect you to the best events, from concerts to festivals, with a simple and secure booking process. No hassle, no stress—just pure excitement, delivered straight to you.
+                        <span className='font-bold'>We bring you closer to the moments that matter. 🎟️✨</span>
                     </p>
                 </div>
             </div>
